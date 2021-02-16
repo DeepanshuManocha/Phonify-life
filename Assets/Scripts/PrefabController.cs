99d0a0998732d1d397d6388a2aa53f2e9b639614
DeepanshuMages <78214866+DeepanshuMages@users.noreply.github.com>
2021-02-16 23:36:51 +0530

    Augmention Alphabets with UI and sound

---
+++ Assets/Scripts/PrefabController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabController : MonoBehaviour
+{
+    private GameObject selectedPrefab;
+    private GameObject spawnedPrefab;
+
+    public void SetPrefab(GameObject prefabObject)
+    {
+        selectedPrefab = prefabObject;
+        foreach (GameObject gameObject in GameObject.FindGameObjectsWithTag("Alphabet") )
+        {
+            Destroy(gameObject);
+            spawnedPrefab = null;
+        }
+        foreach (GameObject g in GameObject.FindGameObjectsWithTag("Player"))
+        {
+            if (spawnedPrefab == null)
+            {
+                spawnedPrefab = Instantiate(selectedPrefab) as GameObject;
+                spawnedPrefab.transform.parent = g.transform;
+                spawnedPrefab.transform.position = g.transform.position + Vector3.up * 0.1f;
+                spawnedPrefab.transform.rotation = g.transform.rotation;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/RotateWithSlider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RotateWithSlider : MonoBehaviour
+{
+    public GameObject objectToRotate;
+    public Slider slider;
+
+    private float previousValue;
+
+    void Awake()
+    {
+        this.slider.onValueChanged.AddListener(this.OnSliderChanged);
+
+        this.previousValue = this.slider.value;
+    }
+
+    public void OnSliderChanged(float value)
+    {
+        float delta = value - this.previousValue;
+        this.objectToRotate.transform.Rotate(Vector3.down * delta * 360);
+        //this.objectToRotate.transform.Rotate(new Vector3(0 , -delta*360, 0));
+        this.previousValue = value;
+    }
+}
